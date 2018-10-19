10cfc985357e082d4b797fbe886aba3bb202dbcf
cindyb <cindyloo_home at yahoo.com>
2018-10-19 12:13:47 -0400

    png was not a real png! working now

---
+++ Neighborhood/PickerViewController.swift
@@ -0,0 +1,155 @@
+//
+//  PickerViewController.swift
+//  Neighborhood
+//
+//  Created by Cindy Bishop on 10/17/18.
+//  Copyright © 2018 LoopyLoo. All rights reserved.
+//
+
+import UIKit
+import Macaw
+
+enum IncomeBracket: Int {
+    // do we want an 0 for no-selection?
+    case low = 1
+    case middle = 2
+    case high = 3
+}
+enum RaceIdentity: Int {
+    case black = 1
+    case brown = 2
+    case pink = 3
+    case offwhite = 4
+    case red = 5
+}
+enum Choice: Int {
+    case food = 1
+    case bar = 2
+    case people = 3
+}
+enum ChoiceAmount: Int {
+    case poor = 1
+    case fair = 2
+    case great = 3
+}
+enum DistanceFromMIT: Int {
+    case under10 = 1
+    case under20 = 2
+    case under30 = 3
+    case above30 = 4
+}
+
+enum Persona : Int {
+    case FACSTAFF = 0
+    case STUDENT = 1 // low income,
+    case FELLOW = 2
+}
+
+class PickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+
+    @IBOutlet weak var applyButton: UIButton!
+    @IBOutlet weak var distancePicker: UIPickerView!
+    @IBOutlet weak var incomePicker: UIPickerView!
+    @IBOutlet weak var racePicker: UIPickerView!
+    
+    // @IBOutlet weak var interestsPicker: UIPickerView!
+    // @IBOutlet weak var mirrordSlider: UISlider!
+    
+    // var personaDataSource = [ "Faculty", "Fellow","Staff", "Student","Visitor"]
+    var incomeDataSource = [ "under 50k", "50k-75k","80k-100k","above 100k", "above 200k"]
+    var raceDataSource = [ "Asian", "Black or African American", "Latino or Hispanic", "Native American (not in census)",  "White alone, not Hispanic/Latino", "Other"]
+    var distanceDataSource = [ "5 miles", "10 miles","20 miles", "above 20"]
+    var educationDataSource = [ "no college degree or equivalent", "college degree", "advanced degree"]
+    var interestsDataSource = [ "safety", "bikeable", "urban"]
+    
+    private var persona = Persona.STUDENT
+    private var income = IncomeBracket.middle
+    private var race = RaceIdentity.red
+    private var interests = Choice.people
+    private var distance = DistanceFromMIT.under10
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        distancePicker.dataSource = self
+        distancePicker.delegate = self
+        incomePicker.dataSource = self
+        incomePicker.delegate = self
+        racePicker.dataSource = self
+        racePicker.delegate = self
+        /*interestsPicker.dataSource = self
+        interestsPicker.delegate = self*/
+
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == distancePicker {
+            return distanceDataSource.count;
+        } else if pickerView == incomePicker {
+            return incomeDataSource.count
+        } else if pickerView == racePicker {
+            return raceDataSource.count
+        } /* else if pickerView == interestsPicker {
+            return interestsDataSource.count
+        } */
+        return 0
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        if pickerView == distancePicker {
+            return distanceDataSource[row]
+        } else if pickerView == incomePicker {
+            return incomeDataSource[row]
+        } else if pickerView == racePicker {
+            return raceDataSource[row]
+        } /*else if pickerView == interestsPicker {
+            return interestsDataSource[row]
+        }*/
+        return "none"
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    { //check all of these to map themp properly
+        if pickerView == distancePicker {
+            if(row == 0){
+                distance = DistanceFromMIT.under10;
+            }else if(row == 1) {
+                distance = DistanceFromMIT.under20;
+            }else if(row == 2) {
+                distance = DistanceFromMIT.under30;
+            } else{
+                distance = DistanceFromMIT.above30;
+            }
+        }  else if pickerView == incomePicker {
+            if(row == 0) {
+                income = IncomeBracket.low;
+            } else if(row == 1 || row == 2) {
+                income = IncomeBracket.middle;
+            } else if(row == 3 || row == 4){
+                income = IncomeBracket.high;
+            }
+        } else if pickerView == racePicker {
+            return race = RaceIdentity.red
+        }
+    }
+    
+    func applyProfileInfo() {
+        // get settings and set up UI visuals
+        //which colors over what tracts
+    }
+
+    @IBAction func closePopover(_ sender: Any) {
+        self.view.removeFromSuperview()
+    }
+    
+    @IBAction func doSomething(sender: UIButton) {
+        if sender == applyButton {
+            applyProfileInfo()
+            self.view.removeFromSuperview()
+            
+        }
+    }
+}
--- Neighborhood/ViewController.swift
+++ Neighborhood/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import SceneKit
 import ARKit
 
+
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
@@ -35,10 +36,15 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
+        guard let referenceMaps = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else {
+            fatalError("Missing expected asset catalog resources.")
+        }
+        configuration.detectionImages = referenceMaps
         // Run the view's session
-        sceneView.session.run(configuration)
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
     }
+
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
@@ -49,14 +55,88 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
-/*
     // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if !(anchor is ARImageAnchor) {
+            return
+        }
+        let imageAnchor = anchor as? ARImageAnchor
+        
+        let referenceImage = imageAnchor?.referenceImage
+        DispatchQueue.main.async {
+
+            let tempCollageView = UIImageView.init(frame: CGRect(x: 0, y: 0, width: 900, height: 900))
+            tempCollageView.layer.bounds = CGRect(x: -450, y: -450, width: tempCollageView.frame.size.width, height: tempCollageView.frame.size.height)
+            tempCollageView.image = UIImage(data: UIImage(named: "mapwork.png")?.pngData() ?? Data())
+
+            let imageMaterial = SCNMaterial()
+            imageMaterial.diffuse.contents = UIImage(named: "mapwork.png")
+            imageMaterial.isDoubleSided = true
+            imageMaterial.diffuse.contents = tempCollageView.layer
+            // Create a plane to visualize the initial position of the detected image.
+            let planeCollage = SCNPlane(width: referenceImage!.physicalSize.width,
+                                 height: referenceImage!.physicalSize.height)
+            planeCollage.materials = [imageMaterial]
+            
+            
+            
+
+            let planeCollageNode = SCNNode(geometry: planeCollage)
+            planeCollageNode.opacity = 0.5
+            planeCollageNode.eulerAngles.x = -.pi / 2
+
+            
+            let plane = SCNPlane(width: referenceImage!.physicalSize.width,
+                                        height: referenceImage!.physicalSize.height)
+            let shape = CAShapeLayer()
+            shape.opacity = 0.5
+            shape.lineWidth = 2
+            shape.lineJoin = CAShapeLayerLineJoin.miter
+            shape.strokeColor = UIColor(red: 0, green: 255, blue: 0, alpha: 1).cgColor
+            shape.fillColor = UIColor(red: 0, green: 255, blue: 0, alpha: 1).cgColor
+            
+            let path = UIBezierPath()
+            path.move(to: CGPoint(x: 100, y: 300))
+            path.addLine(to: CGPoint(x: 250, y: 320))
+            path.addLine(to: CGPoint(x: 260, y: 320))
+            path.addLine(to: CGPoint(x: 200, y: -1000))
+            path.addLine(to: CGPoint(x: 100, y: -1000))
+            path.close()
+            shape.path = path.cgPath
+            
+            let tempView = UIView.init(frame: CGRect(x: 0, y: 0, width: 900, height: 900))
+            tempView.layer.bounds = CGRect(x: -450, y: -450, width: tempView.frame.size.width, height: tempView.frame.size.height)
+            tempView.layer.addSublayer(shape)
+            tempView.backgroundColor = UIColor(red: 0, green: 255, blue: 0, alpha: 0.0)
+            let newMaterial = SCNMaterial()
+            newMaterial.isDoubleSided = true
+            newMaterial.diffuse.contents = tempView.layer
+            plane.materials = [newMaterial]
+            
+            
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.opacity = 0.5
+            planeNode.eulerAngles.x = -.pi / 2
+            /*
+             `SCNPlane` is vertically oriented in its local coordinate space, but
+             `ARImageAnchor` assumes the image is horizontal in its local space, so
+             rotate the plane to match.
+             */
+
+            
+            /*
+             Image anchors are not tracked after initial detection, so create an
+             animation that limits the duration for which the plane visualization appears.
+             */
+            //planeNode.runAction(self.imageHighlightAction)
+            
+            // Add the plane visualization to the scene.
+            node.addChildNode(planeCollageNode)
+            //node.addChildNode(planeNode)
+        }
     }
-*/
+    
+    
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
